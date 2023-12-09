5919532271132e78e8fae78f8c28ba5678faad12
RokastXD <86414172+RokastXD@users.noreply.github.com>
2023-12-09 05:19:12 +0300

    remained cin filter and table correction

--- tic-tac-toe-9x9/tic-tac-toe-9x9.cpp
+++ tic-tac-toe-9x9/tic-tac-toe-9x9.cpp
@@ -2,10 +2,12 @@
 #include <string>
 using namespace std;
 
-char square[9] = {'-', '-', '-' , '-' , '-' , '-' , '-' , '-' , '-' };
+char square[9] = {'1', '2', '3' , '4' , '5' , '6' , '7' , '8' , '9' };
 char squareMatrix[9][9];
 
-int checkwin(int matrix);
+int checkWin(int matrix);
+int checkSquare(int matrix);
+void fillSquare(int matrix, int choice, char playerMark);
 void board();
 
 int main()
@@ -29,11 +31,14 @@ int main()
         cin >> matrix;
         cout << "Player " << player << ", enter a number of cell:  ";
         cin >> choice;
-
+        matrix -= 1; choice -= 1;
         mark=(player == 1) ? 'X' : 'O';
+        
 
-        if (1 <= choice && choice <= 9 && 1 <= matrix && matrix <= 9) {
-            squareMatrix[matrix - 1][choice - 1] = mark;
+        if (0 <= choice && choice <= 8 && 0 <= matrix && matrix <= 8 && (squareMatrix[matrix][choice] != 'X' && squareMatrix[matrix][choice] != 'O')) {
+            squareMatrix[matrix][choice] = mark;
+            if (checkSquare(matrix) == 1)
+                fillSquare(matrix, choice, mark);
         }
         else
         {
@@ -44,7 +49,10 @@ int main()
             cin.ignore();
             cin.get();
         }
-        game=checkwin(matrix);
+
+        
+
+        game=checkWin(matrix);
 
         player++;
     }while(game==-1);
@@ -54,19 +62,62 @@ int main()
         cout<<"==>\aPlayer "<<--player<<" win ";
     else
     {
-        for (int i = 1; i <= 9; i++) {
-            for (int j = 1; j <= 9; j++) {
-                cout << squareMatrix[i - 1][j - 1];
-            }
-            cout << endl;
-        }
-    
         cout<<"==>\aGame draw";
     }
 
     cin.ignore();
     cin.get();
     return 0;
+}
+
+
+
+void fillSquare(int matrix, int choice, char playerMark) {
+    for (int i = 0; i < 9; i++) {
+        squareMatrix[matrix][i] = playerMark;
+        square[matrix] = playerMark;
+    }
+}
+
+/*********************************************
+    FUNCTION TO RETURN CELL STATUS
+    1 FOR CELL IS OVER WITH RESULT
+    -1 FOR CELL IS IN PROGRESS
+    O CELL IS OVER AND NO RESULT
+**********************************************/
+
+int checkSquare(int matrix) {
+    if (squareMatrix[matrix][0] == squareMatrix[matrix][1] && squareMatrix[matrix][1] == squareMatrix[matrix][2])
+
+        return 1;
+    else if (squareMatrix[matrix][3] == squareMatrix[matrix][4] && squareMatrix[matrix][4] == squareMatrix[matrix][5])
+
+        return 1;
+    else if (squareMatrix[matrix][6] == squareMatrix[matrix][7] && squareMatrix[matrix][7] == squareMatrix[matrix][8])
+
+        return 1;
+    else if (squareMatrix[matrix][0] == squareMatrix[matrix][3] && squareMatrix[matrix][3] == squareMatrix[matrix][6])
+
+        return 1;
+    else if (squareMatrix[matrix][1] == squareMatrix[matrix][4] && squareMatrix[matrix][4] == squareMatrix[matrix][7])
+
+        return 1;
+    else if (squareMatrix[matrix][2] == squareMatrix[matrix][5] && squareMatrix[matrix][5] == squareMatrix[matrix][8])
+
+        return 1;
+    else if (squareMatrix[matrix][0] == squareMatrix[matrix][4] && squareMatrix[matrix][4] == squareMatrix[matrix][8])
+
+        return 1;
+    else if (squareMatrix[matrix][2] == squareMatrix[matrix][4] && squareMatrix[matrix][4] == squareMatrix[matrix][6])
+
+        return 1;
+    else if (squareMatrix[matrix][0] != '1' && squareMatrix[matrix][1] != '2' && squareMatrix[matrix][2] != '3'
+        && squareMatrix[matrix][3] != '4' && squareMatrix[matrix][4] != '5' && squareMatrix[matrix][5] != '6'
+        && squareMatrix[matrix][6] != '7' && squareMatrix[matrix][7] != '8' && squareMatrix[matrix][8] != '9')
+
+        return 0;
+    else
+        return -1;
 }
 
 /*********************************************
@@ -76,42 +127,41 @@ int main()
     O GAME IS OVER AND NO RESULT
 **********************************************/
 
-int checkwin(int matrix)
+int checkWin(int matrix)
 {
  
-
-
-    if (squareMatrix[matrix][1] == squareMatrix[matrix][2] && squareMatrix[matrix][2] == squareMatrix[matrix][3])
+    if (square[0] == square[1] && square[1] == square[2])
 
         return 1;
-    else if (squareMatrix[matrix][4] == squareMatrix[matrix][5] && squareMatrix[matrix][5] == squareMatrix[matrix][6])
+    else if (square[3] == square[4] && square[4] == square[5])
 
         return 1;
-    else if (squareMatrix[matrix][7] == squareMatrix[matrix][8] && squareMatrix[matrix][8] == squareMatrix[matrix][9])
+    else if (square[6] == square[7] && square[7] == square[8])
 
         return 1;
-    else if (squareMatrix[matrix][1] == squareMatrix[matrix][4] && squareMatrix[matrix][4] == squareMatrix[matrix][7])
+    else if (square[0] == square[3] && square[3] == square[6])
 
         return 1;
-    else if (squareMatrix[matrix][2] == squareMatrix[matrix][5] && squareMatrix[matrix][5] == squareMatrix[matrix][8])
+    else if (square[1] == square[4] && square[4] == square[7])
 
         return 1;
-    else if (squareMatrix[matrix][3] == squareMatrix[matrix][6] && squareMatrix[matrix][6] == squareMatrix[matrix][9])
+    else if (square[2] == square[5] && square[5] == square[8])
 
         return 1;
-    else if (squareMatrix[matrix][1] == squareMatrix[matrix][5] && squareMatrix[matrix][5] == squareMatrix[matrix][9])
+    else if (square[0] == square[4] && square[4] == square[8])
 
         return 1;
-    else if (squareMatrix[matrix][3] == squareMatrix[matrix][5] && squareMatrix[matrix][5] == squareMatrix[matrix][7])
+    else if (square[2] == square[4] && square[4] == square[6])
 
         return 1;
-    else if (squareMatrix[matrix][0] != '1' && squareMatrix[matrix][1] != '2' && squareMatrix[matrix][2] != '3'
-                    && squareMatrix[matrix][3] != '4' && squareMatrix[matrix][4] != '5' && squareMatrix[matrix][5] != '6'
-                  && squareMatrix[matrix][6] != '7' && squareMatrix[matrix][7] != '8' && squareMatrix[matrix][8] != '9')
+    else if (square[0] != '1' && square[1] != '2' && square[2] != '3'
+        && square[3] != '4' && square[4] != '5' && square[5] != '6'
+        && square[6] != '7' && square[7] != '8' && square[8] != '9')
 
         return 0;
     else
         return -1;
+
 }
 
 
@@ -135,7 +185,7 @@ void board()
             cout << " ";
             for (int j = 0; j <= 2; j++) {
                 for (int k = 0; k <= 2; k++) {
-                    cout << '-'; //squareMatrix[j + matrixK][k + numK];
+                    cout << squareMatrix[j + matrixK][k + numK];
                     if (j == 2 && k == 2) break;
                     cout << " | ";
                 }
